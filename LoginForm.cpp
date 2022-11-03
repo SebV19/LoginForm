1300961fc66d1211759ffb3a704782b47a4c7b56
SebV19 <litoiu.sebastian19@gmail.com>
2022-11-03 18:26:58 +0200

    2

--- LoginForm.cpp
+++ LoginForm.cpp
@@ -1,74 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <sstream>
+
 
 using namespace std;
 
 int answer = 0;
-
-void signUp(){
-
-    ofstream UserList("users_list.csv", ios_base::app);
-    char username[20]; 
-    char psswd[20];
-    int age;
-    char height[20];
-
-    cout << "Enter Your Username: \n "; cin >> username;
-    cout << "Enter your Password: \n"; cin >> psswd;
-    cout << "Enter your age: \n"; cin >> age;
-    cout << "Enter you height: \n"; cin >> height;
-    
-    UserList << username << ", " << psswd << ", " << age << ", " << height << ". \n";
-    UserList.close();
-
-    cout << "Registration Complete \n";
-
-
-}
-
-int login(){
-
-    fstream file;
-
-    file.open("user_list.csv", ios::in);
-    char username[20];
-    char psswd[20];
-
-    cout << "Enter your username: "; cin >> username; 
-    cout << "Enter your password: "; cin >> psswd;
-
-    vector<string> row;
-    string line, word, temp;
-
-    while(file >> temp){
-        row.clear();
-        getline(file, line);
-        stringstream s(line);
-
-        while(getline (s, word, ',') ){
-
-            row.push_back(word);
-
-        }
-
-        username = stoi(row[0]);
-
-
-            
-
-    }
-
-
-
-    return 0;
-}
-
-void displayUser_Page(){
-    cout << "Aici afisezi";
-}
-
-
 
 void menu(){
     switch (answer)
@@ -92,6 +31,101 @@ void menu(){
     } 
 
 }
+
+
+void signUp(){
+
+    ofstream UserList("users_list.csv", ios_base::app);
+    char username[20]; 
+    char psswd[20];
+    int age;
+    char height[20];
+
+    cout << "Enter Your Username: \n "; cin >> username;
+    cout << "Enter your Password: \n"; cin >> psswd;
+    cout << "Enter your age: \n"; cin >> age;
+    cout << "Enter you height: \n"; cin >> height;
+    
+    UserList << username << ", " << psswd << ", " << age << ", " << height << ". \n";
+    UserList.close();
+
+    cout << "Registration Complete \n \n";
+
+    menu();
+
+}
+
+void ask2signUp(){
+
+        cout << "Do you want to make an account? Y/N \n";
+        char ans;
+        cout << "Your answer: "; cin >> ans;
+
+        if(ans == 'y')
+            signUp();
+
+        if(ans == 'n'){
+            cout << "Thank You/n ";
+            exit(EXIT_SUCCESS);
+
+        }
+
+        if(ans != 'y' || ans != 'n'){
+            cout << "Use only characters 'y for YES and 'n' for NO. \n \n";
+            ask2signUp();        
+        }
+
+
+
+}
+
+
+int login(){
+
+    fstream file;
+
+    file.open("users_list.csv", ios::in);
+    string username;
+    string psswd;
+    int count = 0;
+
+    cout << "Enter your username: "; cin >> username; 
+    cout << "Enter your password: "; cin >> psswd;
+
+    vector<string> row;
+    string line, word, temp;
+
+    while(!file.eof()){
+
+        row.clear();
+        getline(file, line);
+        stringstream s(line);
+    
+        while(getline (s, word, ',' ) ){
+            row.push_back(word);
+
+        }
+        if(username.compare(row[0]) == 0 && psswd.compare(row[1]) == 0){
+            count = 1;
+            cout << "You are in \n";
+            break;
+        }
+
+        }
+
+        if(count == 0){
+            cout << "There is no user with this username and password \n";
+            ask2signUp();
+        }
+
+    
+    return 0;
+}
+
+void displayUser_Page(){
+    cout << "Aici afisezi";
+}
+
 
 
 int main() {
